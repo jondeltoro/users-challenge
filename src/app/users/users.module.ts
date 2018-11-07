9a19d2640a22cda80aac7f3e08e11be1c7628074
Jonathan Del Toro <jonathandeltoro@outlook.com>
2018-11-07 00:18:00 -0800

    home component + user module + user home component + basic routing

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UsersRoutingModule } from './users-routing.module';
+import { UsersHomeComponent } from './users-home/users-home.component';
 
 @NgModule({
   imports: [
     CommonModule,
     UsersRoutingModule
   ],
-  declarations: []
+  declarations: [UsersHomeComponent]
 })
 export class UsersModule { }
