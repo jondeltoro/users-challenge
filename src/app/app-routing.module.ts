32d85ea748985e9615d014124eedca1422e7813e
Jonathan Del Toro <jonathandeltoro@outlook.com>
2018-11-08 02:47:05 -0800

    app routing added lazy load for users + added dependencies on users module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
     path: '', pathMatch: 'full', redirectTo: 'home'
   }, {
+    path: 'users', loadChildren: '../app/users/users.module#UsersModule'
+  }, {
     path: 'home', pathMatch: 'full', component: HomeComponent
-  },
+  }, {
+    path: 'login', pathMatch: 'full', component: LoginComponent
+  }
 ];
-
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,14 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersHomeComponent } from './users-home/users-home.component';
+import { UsersApiService } from './users-api.service';
+import { UsersListPaginationComponent } from './users-home/users-list-pagination/users-list-pagination.component';
+import { SharedModule } from '../shared/shared.module';
+import { UsersCreateComponent } from './users-create/users-create.component';
+import { UsersListComponent } from './users-list/users-list.component';
+import { UsersOpenProfileComponent } from './users-open-profile/users-open-profile.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    UsersRoutingModule
+    UsersRoutingModule,
+    SharedModule
   ],
-  declarations: [UsersHomeComponent]
+  declarations: [
+    UsersHomeComponent,
+    UsersCreateComponent,
+    UsersListComponent,
+    UsersListPaginationComponent,
+    UsersCreateComponent,
+    UsersListComponent,
+    UsersOpenProfileComponent
+  ],
+  exports: [],
+  providers: [UsersApiService]
 })
 export class UsersModule { }
