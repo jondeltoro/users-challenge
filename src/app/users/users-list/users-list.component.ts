3c378be30e7ec3e51323b35b4c29c28efb6cb6e2
Jonathan Del Toro <jonathandeltoro@outlook.com>
2018-11-08 16:21:31 -0800

    user list + user search + user home refactor

--- src/app/users/users-list/users-list.component.ts
+++ src/app/users/users-list/users-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef, OnDestroy } from '@angular/core';
-import { Router } from '@angular/router';
+import { FormControl } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap, map } from 'rxjs/operators';
 import { UsersApiService } from '../users-api.service';
 import { UserListModel } from '../models/user-list.model';
 import { UserModel } from '../models/user.model';
@@ -14,7 +16,12 @@ import { UserModel } from '../models/user.model';
 export class UsersListComponent implements OnInit, OnDestroy {
   private userListSubscription = new Subscription();
   private userDeleteSubscription = new Subscription();
+  private userSearchSubscription = new Subscription();
   public loading = false;
+  public searchMode = false;
+  public searchInput: FormControl = new FormControl();
+  public searchText = '';
+
   public userList: UserListModel = {
     data: [],
     page: 0,
@@ -27,15 +34,35 @@ export class UsersListComponent implements OnInit, OnDestroy {
     private usersApi: UsersApiService,
     private changeDetector: ChangeDetectorRef,
     private router: Router,
+    private route: ActivatedRoute,
   ) { }
 
   ngOnInit() {
-    this.getUserListByPageNumber(1);
+    const data = this.route.snapshot.data;
+    if (data && data.searchMode) {
+      this.searchMode = true;
+    }
+    if (this.searchMode) {
+      this.getFullUserList();
+    } else {
+      this.getUserListByPageNumber(1);
+    }
+    this.userSearchSubscription = this.searchInput.valueChanges.pipe(
+      debounceTime(400),
+      distinctUntilChanged(),
+      map((keyword: string) => {
+        return keyword.toLowerCase();
+      })
+    ).subscribe(keyword => {
+      this.searchText = keyword;
+      this.changeDetector.markForCheck();
+    });
   }
 
   ngOnDestroy() {
     this.userListSubscription.unsubscribe();
     this.userDeleteSubscription.unsubscribe();
+    this.userSearchSubscription.unsubscribe();
   }
 
   getUserListByPageNumber(page = 1) {
@@ -49,6 +76,23 @@ export class UsersListComponent implements OnInit, OnDestroy {
       },
       error => this.loading = false
     );
+  }
+
+  getFullUserList() {
+    this.userListSubscription.unsubscribe();
+    this.loading = true;
+    const errorHandler = () => this.loading = false;
+    const pullFullList = (count: number) => {
+      this.userListSubscription = this.usersApi.getFullUserList(count).subscribe(
+        (userList: UserListModel) => {
+          this.loading = false;
+          this.userList = userList;
+          this.changeDetector.markForCheck();
+        },
+        errorHandler
+      );
+    };
+    this.userListSubscription = this.usersApi.getUserListRecordCount().subscribe(pullFullList, errorHandler);
   }
 
   changePageHandler(page: number) {
@@ -67,6 +111,7 @@ export class UsersListComponent implements OnInit, OnDestroy {
       this.userDeleteSubscription = this.usersApi.deleteUser(user.id).subscribe(
         () => {
           this.userList.data = this.userList.data.filter(storedUser => storedUser !== user);
+          this.userList.total--;
           this.loading = false;
           this.changeDetector.markForCheck();
         },
@@ -75,4 +120,12 @@ export class UsersListComponent implements OnInit, OnDestroy {
     }
   }
 
+  filter(users: UserModel[]) {
+    const keyword = this.searchText;
+    if (keyword) {
+      return users.filter(user => (user.first_name.toLocaleLowerCase().indexOf(keyword) >= 0
+        || user.last_name.toLocaleLowerCase().indexOf(keyword) >= 0));
+    }
+    return users;
+  }
 }
