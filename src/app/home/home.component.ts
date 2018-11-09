0be953a5ff9b00a59bb3fcb6d4206636760ff062
Jonathan Del Toro <jonathandeltoro@outlook.com>
2018-11-08 16:22:11 -0800

    home refactor + login ui

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { SessionApiService } from '../session-api.service';
+import { SessionModel } from '../models/session.model';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  private activeSession: SessionModel = null;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private sessionApi: SessionApiService,
+  ) {
+    this.activeSession = sessionApi.sessionStatus();
+    if (!this.activeSession || !this.activeSession.token) {
+      this.router.navigate(['/login']);
+    }
+  }
 
   ngOnInit() {
   }
 
+  logout() {
+    this.sessionApi.logout();
+    this.router.navigate(['/login']);
+  }
 }
